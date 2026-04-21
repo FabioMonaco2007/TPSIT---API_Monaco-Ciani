f73f704ab23f3d6925a0a057d0b330d881c253a7
Fabio Monaco <fabiomonaco007@gmail.com>
2026-04-21 09:02:37 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
+//server.js
 const express = require("express");
 const app = express();
 const playersRoutes = require("./Routes/players");
 
+//MIDDLEWARE per il parsing del JSON
+//Fondamentale: permette ad Express di leggere i dati inviati nel corpo della richiesta (req.body)
 app.use(express.json());
 
-// Rotta per la rosa della Juventus
+//ROUTING: Colleghiamo le rotte dei giocatori al prefisso "/api/players"
+//Questo rende il codice organizzato: tutte le rotte in players.js inizieranno con questo percorso
 app.use("/api/players", playersRoutes);
 
+//Avvio del server su una porta specifica
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Fino alla fine! Server attivo su http://localhost:${PORT}`);
